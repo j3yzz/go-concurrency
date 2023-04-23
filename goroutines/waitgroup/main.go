22c77197607eb01f2da41ffd8a8189532a4105b1
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-04-23 16:57:18 +0330

    change: refactor waitgroup codes

--- goroutines/waitgroup/main.go
+++ goroutines/waitgroup/main.go
@@ -8,17 +8,25 @@ import (
 )
 
 func main() {
-	var wg sync.WaitGroup
-	fmt.Printf("%#v\n", wg)
-
 	args := os.Args[1:]
-	count, err := strconv.Atoi(args[0])
-
-	if err != nil {
-		fmt.Println("error during conversion")
+	if len(args) == 0 {
+		fmt.Println("Please provide a number as an argument.")
 		return
 	}
 
+	count, err := strconv.Atoi(args[0])
+
+	if err != nil {
+		fmt.Printf("Error during conversion: %v\n", err)
+		return
+	}
+
+	printNumbers(count)
+	fmt.Println("\nExiting...")
+}
+
+func printNumbers(count int) {
+	var wg sync.WaitGroup
 	for i := 0; i < count; i++ {
 		wg.Add(1)
 		go func(x int) {
@@ -26,8 +34,6 @@ func main() {
 			fmt.Printf("%d ", x)
 		}(i)
 	}
-	fmt.Printf("\n")
-	fmt.Printf("%#v\n", wg)
+
 	wg.Wait()
-	fmt.Println("exiting...")
 }
